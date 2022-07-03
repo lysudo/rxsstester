604b98ba3a1d4a03031abcc0bfeec72708ba823f
hamza mohammad <90393603+at-r00t@users.noreply.github.com>
2022-07-03 14:18:13 +0200

    Add files via upload
    add features and fix bugs

--- getparams.py
+++ getparams.py
@@ -1,21 +1,86 @@
 #!/bin/python3
-import re,sys
+
+import re
+import sys
+import getopt
 from urllib.parse import urlparse
+from os.path import exists
 
-args = sys.argv
-if len(args) < 2:
-	print("target file not provided ..")
-	exit()
 
-with open(args[1]) as file:
-	results = []
-	for line in file:
+def message():
+	print(f"""
 
-		url = urlparse(f"{line}")._replace(fragment="").geturl()
-		params = re.findall(r"(?:\?|\&)(?P<key>[\w]+)=(?P<value>[\w+,.-.%]+)(?:\:?)(?P<option>[\w,]*)",url)
+extract all params ;-)
+usage: {__file__} [OPTIONS]
+OPTIONS: 
+	-f <targetfile> ,contains line separated urls/endpoits
+	-o <outputfile> ,write to specific file 
+	--silent ,don't show results""")
 
-		for match in params:
+
+#in case no argv privded
+if len(sys.argv) < 2:
+		print("please provide args ..")
+		message()
+		exit()
+
+
+def main(argv):
+
+	try:
+		opts,values = getopt.getopt(argv,"hf:o:",["silent"])
+	except getopt.GetoptError:
+		message()
+		exit()
+
+	try:
+		if opts[0][0] == '-h':
+			message()
+			exit()
+	except IndexError:
+		print(f"usage: {__file__} -f <inputfile>")
+	
+	#settings
+	inputfile = ""
+	outputfile = ""
+	silent = False
+
+	#set settings
+	for key,value in opts:
+		if key == '-f':
+			inputfile = value
+		elif key == '-o':
+			outputfile = value
+		elif key == "--silent":
+			silent = True
+
+	if not exists(inputfile):
+		print(f'file "{inputfile}" not exist ..')
+		exit()
+	
+	#READ WRITE FILES
+	with open(inputfile, 'r',encoding="utf-8") as i:
 		
-			if match[0] not in results:
-				results.append(match[0])
-				print(match[0])
+		results = []
+		for line in i:
+			url = urlparse(f"{line}")._replace(fragment="").geturl()
+			regex = r"(?:\?|\&)(?P<key>[\w]+)=(?P<value>[\w+,.-.%]+)(?:\:?)(?P<option>[\w,]*)"
+			params = re.findall(regex,url)
+
+			for match in params:
+				if match[0] not in results:
+					results.append(match[0])
+					if not silent:
+						print(match[0])
+
+		# if user specified file
+		if outputfile:
+			with open(outputfile,'w',encoding="utf-8") as o:
+				for result in results:
+					o.write(f"{result}\n")
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
+
+
+
--- rxsstester.py
+++ rxsstester.py
@@ -1,39 +1,157 @@
 #!/bin/python3
 
 import requests,sys
+import re,sys,getopt
 from urllib.parse import urlparse
+from os.path import exists
 
-#suppress warnings
+
+######## suppress urllib warnings
 from urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+########
 
-args = sys.argv
+def message(notfound="", required=""):
+	
+	#ERROR MESSAGES
+	if notfound:
+		print(f"<{notfound}> file not found ..")
 
-if len(args) < 3:
-	print(f"please provide target urls and params files ..\nUSAGE: {args[0]} <urls> <params>")
-	exit()
+	if required:
+		print(f"<{required}> file is (required) ..")
 
-if args[1] == "-url" :
-	url = args[2]
-	try:
-		with open(args[3],'r') as params:
-				for param in params:
-					r = requests.get(url,params={param:"FUZZ"},verfiy=False)
-					if "FUZZ" in r.text :
-						print(f"[+] {url} ######>>>>###### {param}") 
-	except IndexError:
-		print(f"params file not privded ..\nUSAGE: {args[0]} <urls> <params>")
+
+	#USAGE MESSAGE
+	print(f"""\nusage: {__file__} -f <inputfile>
+
+	OPTIONS: 
+	-f / --targetsfile <file> : line separated file contains target endpoits
+	--url : a singl endpoint to test against
+	-p / --paramsfile <file> : line separated file contains list of parameters to test
+	-o <outputfile> ,write to specific file 
+	--silent : sshh ,don't show results""")
+
+#in case no argv privded
+if len(sys.argv) < 2:
+		print("please provide args ..")
+		message()
 		exit()
-	exit()
 
-with open(args[1],'r') as urls:
-	for url in urls:
-		url = urlparse(url).path
-		with open(args[2],'r') as params:
-			for param in params:
-				try:
-					r = requests.get(url,params={param:"FUZZ"},verify=False)
-					if "FUZZ" in r.text :
-						print(f"[+] {url} ######>>>>###### {param}") 
-				except:
-					pass
+def fuzz(target ,paramsfile,output ,isFile=True,write=False ,silent=False):
+	targets = []
+	params = []
+	results = []
+
+	#######################
+	#if it's a single URL '--url'
+	if not isFile:
+		targets.append(urlparse(target).path)
+		try:	
+			with open(paramsfile, 'r',encoding="utf-8") as p:
+				params = list(p)
+		except FileNotFoundError:
+			message(required="")
+			exit()
+
+	#if it's a file '-f'
+	else:
+		try:
+			with open(target, 'r',encoding="utf-8") as t:
+				targets = list(t)
+		except FileNotFoundError:
+			message(required="target")
+			exit()
+
+		try:	
+			with open(paramsfile, 'r',encoding="utf-8") as p:
+				params = list(p)
+		except FileNotFoundError:
+			message(required="params")
+			exit()
+	########################
+	
+	for url in targets:
+		url = urlparse(url)
+		url = url.scheme+"://"+url.netloc+url.path if isFile else target
+		if not silent:
+			print(f"[!] testing for {url}")
+
+		# the whole thing ..
+		for param in params:
+			try:
+				r = requests.get( url ,params={param:"FUZZ"},verify=False)
+			except requests.exceptions.ConnectionError:
+				print("[X] please check your connection ..")
+				exit()
+			except requests.exceptions.InvalidSchema:
+				print("[X] InvalidSchema: please check if the url have the proper schema [http/https]")
+				exit()
+
+			if "FUZZ" in r.text and param not in results:
+				results.append("%s => %s\n-----"% (url,param))
+				if not silent:
+					print(f"[+] => {param}")
+
+	if write and results:
+		with open(output,'w',encoding="utf-8") as o:
+			for result in results:
+				o.write(result)
+
+def main(argv):
+
+	try:
+		opts,values = getopt.getopt(argv,"hf:p:o:",["targetsfile=","paramsfile=","url=","silent"])
+	
+	except getopt.GetoptError:
+		message()
+		exit()
+
+	try:
+		if opts[0][0] == '-h':
+			message()
+			exit()
+
+	except IndexError:
+		message()
+
+	#settings
+	target = ""
+	paramsfile = ""
+	output = "rxssresults.txt" #default
+	isFile = True #whether it's a file '-f' or single url '--url'
+	write = False
+	silent = False
+
+	
+	for key,value in opts:
+
+		if key == "-f" or key == "--targetfile":
+			if not exists(value):
+				message(notfound="target")
+				exit()
+			target = value
+
+		elif key == "--url":
+			target = value
+			isFile = False
+
+		elif key == "-p" or key == "--paramsfile":
+			if not exists(value):
+				messag(notfound="params")
+				exit()
+			paramsfile = value
+
+
+		elif key == "-o":
+			output = value
+			write = True
+
+		elif key == "--silent":
+			silent = True
+
+
+	fuzz(target,paramsfile,output,isFile=isFile,write=write,silent=silent)
+
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
